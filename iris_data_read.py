6bafd72c6412e93e980961c50128443f972a50ac
Sangwon <kimssw0202@naver.com>
2023-01-09 16:56:08 +0900

    Iris Data pandas 구현

---
+++ iris_data_read.py
@@ -0,0 +1,12 @@
+from sklearn.datasets import load_iris
+import numpy as np
+import pandas as pd
+
+Iris = load_iris()
+
+Iris_Data = pd.DataFrame(data= np.c_[Iris['data'], Iris['target']],
+                         columns= Iris['feature_names'] + ['target'])
+Iris_Data['target'] = Iris_Data['target'].map({
+    0: "setosa", 1: "versicolor", 2: "virginica"
+})
+print(Iris_Data)
